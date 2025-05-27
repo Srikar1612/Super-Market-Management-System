224a0b13930c5f1f03ddbfd10595b50010f371bb
Srikar1612 <147256947+Srikar1612@users.noreply.github.com>
2025-05-27 22:25:12 +0530

    Add files via upload

---
+++ UserBean.java
@@ -0,0 +1,95 @@
+package com.bean;
+
+public class UserBean {
+	private String name, password, email, age, gender, phone, status, pid, cid, oid,eid;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getAge() {
+		return age;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public String getEid() {
+		return eid;
+	}
+	
+	public String getPid() {
+		return pid;
+	}
+	
+	public String getCid() {
+		return cid;
+	}
+	
+	public String getOid() {
+		return oid;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public void setStatus(String status) {
+		this.status= status;
+	}
+	
+	public void setEid(String eid) {
+		this.eid = eid;
+	}
+	
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	
+	public void setOid(String oid) {
+		this.oid= oid;
+	}
+
+}
