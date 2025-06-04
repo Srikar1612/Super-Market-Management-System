6071039f07357dcdcbf30c831c055efbee3b990f
Srikar Vinjamuri <147256947+Srikar1612@users.noreply.github.com>
2025-06-04 16:48:28 +0530

    Update Toggle.js

--- WebContent/JS/Toggle.js
+++ WebContent/JS/Toggle.js
@@ -7,10 +7,10 @@ $(document).ready(function () {
       $('.tab-section').hide();
       $(target).show();
     });
-    $('input[type=button][[href^="#"]').click(function (e) {
-        e.preventDefault();
-        var target = $(this).attr('href');
-        $('.tab-section').hide();
-        $(target).show();
+    $('input.Quick-Action-Button').click(function () {
+        var target=$(this).data('target');
+    	if(target){
+    		$('.tab-section').hide();
+    		$(target).show();
       });
   });
