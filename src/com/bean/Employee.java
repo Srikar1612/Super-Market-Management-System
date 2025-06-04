8781eecc9d6bfea807f62184980400e3092b952c
Srikar Vinjamuri <147256947+Srikar1612@users.noreply.github.com>
2025-06-04 18:28:56 +0530

    Add files via upload

---
+++ src/com/bean/Employee.java
@@ -0,0 +1,55 @@
+package com.bean;
+
+public class Employee {
+	private String id,name,age,status,gender,phone;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+}
